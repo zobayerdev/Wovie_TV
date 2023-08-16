34b49c202e815986f805984ffd912b0b38325612
zobayerdev <zobayer.dev@gmail.com>
2023-08-16 16:17:50 +0600

    complete application

--- app/src/main/java/com/trodev/wovietv/MainActivity.java
+++ app/src/main/java/com/trodev/wovietv/MainActivity.java
@@ -1,6 +1,7 @@
 package com.trodev.wovietv;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -8,9 +9,12 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -22,6 +26,10 @@ import android.view.Window;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.startapp.sdk.adsbase.Ad;
+import com.startapp.sdk.adsbase.StartAppAd;
+import com.startapp.sdk.adsbase.adlisteners.AdEventListener;
+import com.startapp.sdk.adsbase.adlisteners.VideoListener;
 
 import java.util.Objects;
 
@@ -44,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         /*init all drawer layout*/
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigation_view);
+
+        /*start banner ads on main activity*/
+        StartAppAd.showAd(this);
 
         /*init views*/
         smoothBottomBar = findViewById(R.id.bottombar);
@@ -106,6 +117,8 @@ public class MainActivity extends AppCompatActivity {
                     getWindow().setNavigationBarColor(Color.parseColor("#000000"));
                     getWindow().setStatusBarColor(Color.parseColor("#000000"));
 
+                    /*start ads on main activity*/
+                    StartAppAd.showAd(MainActivity.this);
 
                 }
 
@@ -120,6 +133,28 @@ public class MainActivity extends AppCompatActivity {
                     smoothBottomBar.setBarBackgroundColor(Color.parseColor("#000000"));
                     getWindow().setNavigationBarColor(Color.parseColor("#000000"));
                     getWindow().setStatusBarColor(Color.parseColor("#000000"));
+
+                    /*start ads on main activity*/
+                    StartAppAd video = new StartAppAd(MainActivity.this);
+                    video.setVideoListener(new VideoListener() {
+                        @Override
+                        public void onVideoCompleted() {
+
+                        }
+                    });
+
+                    video.loadAd(StartAppAd.AdMode.REWARDED_VIDEO, new AdEventListener() {
+                        @Override
+                        public void onReceiveAd(@NonNull Ad ad) {
+                            video.showAd();
+                        }
+
+                        @Override
+                        public void onFailedToReceiveAd(@Nullable Ad ad) {
+
+                        }
+                    });
+
                 }
 
                 if (i == 3) {
@@ -133,11 +168,61 @@ public class MainActivity extends AppCompatActivity {
                     smoothBottomBar.setBarBackgroundColor(Color.parseColor("#000000"));
                     getWindow().setNavigationBarColor(Color.parseColor("#000000"));
                     getWindow().setStatusBarColor(Color.parseColor("#000000"));
+
+                    /*start ads on main activity*/
+                    StartAppAd.showAd(MainActivity.this);
                 }
 
                 return false;
             }
         });
+
+        confirm();
+
+    }
+
+    public void confirm() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle("PRIVACY & CONTENT POLICY");
+        //  alertDialogBuilder.setIcon(R.drawable.ic_delete);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("The app doesn't collect any user personal data as, for example, name, picture or location.\n" +
+                        "\n" +
+                        "• Consequently, the app doesn't share any personal information with any other entity or third parties.\n" +
+                        "\n" +
+                        "Images and Videos entered by the user are sent to the app server in order to be retrieved later by the user himself, and so that the app can offer the functionalities according to its description.\n" +
+                        "\n" +
+                        "We allow third-party companies to serve ads and collect certain anonymous information when you visit our app. These companies may use anonymous information such as your Google Advertising ID, your device type and version, browsing activity, location and other technical data relating to your device, in order to provide advertisements.\n" +
+                        "\n" +
+                        "Do you agreeing with all of them.")
+                .setCancelable(false)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        dialog.cancel();
+                        Toast.makeText(MainActivity.this, "Thanks for your responses", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+
+        // show it
+        alertDialog.show();
+
 
     }
 
@@ -152,6 +237,22 @@ public class MainActivity extends AppCompatActivity {
 
         if (itemId == R.id.menu_contact) {
             Toast.makeText(this, "Contact us", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.menu_telegram) {
+            Intent intent;
+            try {
+                try {
+                    getApplication().getPackageManager().getPackageInfo("org.telegram.messenger", 0);
+                } catch (PackageManager.NameNotFoundException e) {
+                    getApplication().getPackageManager().getPackageInfo("org.thunderdog.challegram", 0);
+                }
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/WovieTVxyz"));
+                startActivity(intent);
+
+            } catch (PackageManager.NameNotFoundException e) {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/WovieTVxyz"));
+                startActivity(intent);
+            }
+
         } else if (itemId == R.id.menu_privacy) {
             Toast.makeText(this, "Privacy Policy", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.menu_share) {
